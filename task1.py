b5bb77cc024c61095cfe8bdc3ba9062e52a8803c
Your Name <hahapupkin@gmail.com>
2022-11-23 10:19:42 +0300

    first commit

---
+++ task1.py
@@ -0,0 +1,16 @@
+"""
+Напишите программу, которая принимает на вход цифру,
+обозначающую день недели, и проверяет,
+является ли этот день выходным.
+"""
+day = int(input("Enter the day number:"))
+
+def checkDay(day):
+    pass
+if 0 < day <= 6:
+    print("yes")
+else:
+    print("no")
+
+checkDay(day)
+
